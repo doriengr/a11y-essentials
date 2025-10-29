8ed1df3f92b8fec5f3cee5ca51cec862fbde6e15
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2025-10-29 18:48:51 +0100

    chore: cleanup code

--- resources/js/components/mainNavigation.js
+++
@@ -1,7 +0,0 @@
-export default () => ({
-    isOpen: false,
-
-    toggle() {
-        return (this.isOpen = !this.isOpen);
-    },
-});
--- resources/js/site.js
+++ resources/js/site.js
@@ -1,5 +1,3 @@
 import Alpine from 'alpinejs';
-import mainNavigation from './components/mainNavigation';
 
 window.Alpine = Alpine;
-Alpine.data('mainNavigation', mainNavigation);
