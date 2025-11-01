f582d4ae1b18bc1b84db9d4b4917eba3246d6f39
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2025-11-01 14:40:31 +0100

    feat: implement form validation for login screen

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
             ]);
     }
 
-    public function store(Request $request)
+    public function registerStore(Request $request)
     {
         $validated = $request->validate([
             'name' => ['required', 'string', 'min:5', 'max:40'],
@@ -51,6 +51,26 @@ class AuthController extends Controller
             ->layout('layouts.default')
             ->with([
                 'title' => 'Login',
+            ]);
+    }
+
+    public function loginStore(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials, $request->filled('remember'))) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/');
+        }
+
+        return back()
+            ->withInput($request->only('email'))
+            ->withErrors([
+                'email' => 'Die angegebenen Anmeldedaten stimmen nicht.',
             ]);
     }
 }
