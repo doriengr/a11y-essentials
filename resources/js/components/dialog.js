30002a66c63fe65df8fa42eecc7d3220bb623b2d
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2025-11-10 14:26:50 +0100

    fix: close dialog on esc

--- resources/js/components/dialog.js
+++ resources/js/components/dialog.js
@@ -3,19 +3,38 @@ export default (options = {}) => ({
     loadResourceRoute: options.loadResourceRoute ?? '',
     isOpen: false,
     resourceLoaded: false,
+    dialog: null,
 
-    toggle() {
-        const dialog = this.$root.querySelector('dialog');
-        if (!dialog) return;
-        this.isOpen = !this.isOpen;
-        this.isOpen ? dialog.showModal() : dialog.close();
-
-        if (this.isOpen) this.setFocus(dialog);
-        if (!this.resourceLoaded && this.isOpen) this.loadResource();
+    init() {
+        this.dialog = this.$root.querySelector('dialog');
+        if (!this.dialog) return;
+        document.addEventListener('keydown', (event) => {
+            if (event.key === 'Escape' && this.isOpen) {
+                this.closeDialog();
+            }
+        })
     },
 
-    setFocus(dialog) {
-        const button = dialog.querySelector('button');
+    toggle() {
+        if (!this.dialog) return;
+        this.isOpen = !this.isOpen;
+
+        if (this.isOpen) {
+            document.body.classList.add('overflow-y-hidden');
+            this.dialog.showModal();
+            this.setFocus();
+
+            if (!this.resourceLoaded) {
+                this.loadResource();
+            }
+        } else {
+            this.closeDialog();
+        }
+    },
+
+    setFocus() {
+        if (!this.dialog) return;
+        const button = this.dialog.querySelector('button');
         if (!button) return;
         button.focus();
     },
@@ -25,9 +44,15 @@ export default (options = {}) => ({
 
         const html = await fetch(this.loadResourceRoute).then(r => r.text());
         const target = this.$root.querySelector('[id^="dialog-target"]');
-        console.log(target);
         if (!target) return;
         target.innerHTML = html;
         this.resourceLoaded = true;
+    },
+
+    closeDialog() {
+        if (!this.dialog) return;
+        this.dialog.close()
+        this.isOpen = !this.isOpen;
+        document.body.classList.remove('overflow-y-hidden');
     }
 });
