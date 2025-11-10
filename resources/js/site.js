dc867c4223c03ae5c918960b7284ff6d2cf4dfc4
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2025-11-10 08:07:33 +0100

    feat: implement dialog

--- resources/js/site.js
+++ resources/js/site.js
@@ -4,6 +4,7 @@ import './stores/navigation';
 
 import accordion from './components/accordion';
 import axeChecker from './components/axeChecker';
+import dialog from './components/dialog';
 import form from './components/form';
 import mainNavigation from './components/mainNavigation';
 import profileNavigation from './components/profileNavigation';
@@ -11,6 +12,7 @@ import sideNavigation from './components/sideNavigation';
 
 Alpine.data('accordion', accordion);
 Alpine.data('axeChecker', axeChecker);
+Alpine.data('dialog', dialog);
 Alpine.data('form', form);
 Alpine.data('mainNavigation', mainNavigation);
 Alpine.data('profileNavigation', profileNavigation);
