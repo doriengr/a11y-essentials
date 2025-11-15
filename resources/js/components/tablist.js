d77b73a02c91f898883f7a435b3ff0d3075a04e9
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2025-11-15 09:21:05 +0100

    feat: implement all functions for tablist

--- resources/js/components/tablist.js
+++ resources/js/components/tablist.js
@@ -1,4 +1,4 @@
-export default (options = {}) => ({
+export default () => ({
     tabs: null,
     panels: null,
     currentLabel: '',
@@ -6,6 +6,8 @@ export default (options = {}) => ({
     init() {
         this.tabs = this.$root.querySelectorAll('[role=tab]');
         this.panels = this.$root.querySelectorAll('[role=tabpanel]');
+
+        this.setFirstTabAsActive();
     },
 
     selectTab(currentLabel) {
@@ -20,5 +22,41 @@ export default (options = {}) => ({
                 panel.classList.remove('hidden')
             }
         });
+    },
+
+    setFirstTabAsActive() {
+        if (!this.tabs || !this.panels) return;
+        const label = this.tabs[0].id.replace('tab-', '');
+        if (!label) return;
+        this.selectTab(label);
+    },
+
+    prevTab() {
+        if (!this.tabs || !this.panels) return;
+        let index = Array.from(this.tabs).findIndex(tab => tab.id === `tab-${this.currentLabel}`);
+        index = index <= 0 ? this.tabs.length - 1 : index - 1;
+        const label = this.tabs[index].id.replace('tab-', '');
+        this.selectTab(label);
+    },
+
+    nextTab() {
+        if (!this.tabs || !this.panels) return;
+        let index = Array.from(this.tabs).findIndex(tab => tab.id === `tab-${this.currentLabel}`);
+        index = index >= this.tabs.length - 1 ? 0 : index + 1; // wrap around
+        const label = this.tabs[index].id.replace('tab-', '');
+        this.selectTab(label);
+    },
+
+    fistTab() {
+        if (!this.tabs || !this.panels) return;
+        const label = this.tabs[0].id.replace('tab-', '');
+        this.selectTab(label);
+    },
+
+    lastTab() {
+        if (!this.tabs || !this.panels) return;
+        const last = this.tabs[this.tabs.length - 1];
+        const label = last.id.replace('tab-', '');
+        this.selectTab(label);
     }
 });
