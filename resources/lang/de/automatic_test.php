86a4ff4439b490296a6e4aa82d768a0118358c8c
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2025-11-23 15:49:24 +0100

    feat: display passes to automatic tests

--- resources/lang/de/automatic_test.php
+++ resources/lang/de/automatic_test.php
@@ -10,6 +10,7 @@ return [
         'wcag2a' => 'Probleme in Konformitätsstufe A',
         'wcag2aa' => 'Probleme in Konformitätsstufe AA',
         'wcag2aaa' => 'Probleme in Konformitätsstufe AAA',
+        'passes' => 'Erfolgreich abgeschlossene Überprüfungen',
     ],
 
     'problem_descriptions' => [
@@ -20,5 +21,6 @@ return [
         'wcag2a' => 'Diese Konformitätsstufe bildet die Basis vieler Regeln bezüglich a11y. Die Probleme sollten auf jeden Fall verbessert werden.',
         'wcag2aa' => 'Wie die Konformitätsstufe A, ist auch dieses Level gesetzlich einzuhalten, sodass auch diese Probleme in Augenschein genommen werden sollten.',
         'wcag2aaa' => 'Diese Konformitätsstufe ist nicht gesetzlich vorgeschrieben, aber verbessert weiterhin die User Experience.',
+        'passes' => 'Folgende Überprüfungen wurden bereits erfolgreich abgeschlossen und müssen nicht weiter angepasst werden.'
     ],
 ];
