09f156e50176bc0305dd23ad505d969cece5ca48
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2025-12-07 12:36:37 +0100

    feat: add code highlights for js and css

--- resources/js/alpine-start.js
+++ resources/js/alpine-start.js
@@ -1,7 +1,11 @@
 import hljs from 'highlight.js/lib/core';
+import css from 'highlight.js/lib/languages/css';
+import javascript from 'highlight.js/lib/languages/javascript';
 import xml from 'highlight.js/lib/languages/xml';
 
+hljs.registerLanguage('css', css);
 hljs.registerLanguage('html', xml);
+hljs.registerLanguage('javascript', javascript);
 
 window.addEventListener('load', () => {
     if (window.Alpine) {
