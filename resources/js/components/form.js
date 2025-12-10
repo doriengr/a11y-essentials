46977e4bb30c725ce2426522e0613f5047f23e72
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2025-12-10 17:23:25 +0100

    feat: add form tracking

--- resources/js/components/form.js
+++ resources/js/components/form.js
@@ -8,5 +8,19 @@ export default (options = {}) => ({
             if (!errorSummary) return;
             errorSummary.focus();
         }
+
+        this.$root.addEventListener('submit', function (e) {
+            window.dataLayer = window.dataLayer || [];
+            window.dataLayer.push({
+                event: 'form_submit',
+                form_id: this.$root.id,
+            });
+
+            // wait for submit to fire gtm event
+            e.preventDefault();
+            setTimeout(() => {
+                this.$root.submit();
+            }, 100);
+        });
     },
 });
