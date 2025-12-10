3715891dbd18e361de0ed96a4068a7129582c562
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2025-12-10 17:34:02 +0100

    fix: update form tracking

--- resources/js/components/form.js
+++ resources/js/components/form.js
@@ -2,11 +2,26 @@ export default (options = {}) => ({
     errorsCount: options.errorsCount ?? 0,
 
     init() {
+        const form = this.$root;
+
         // summary with form errors should retrieve initial focus
         if (this.errorsCount > 0) {
             const errorSummary = document.getElementById('form-errors-summary');
             if (!errorSummary) return;
             errorSummary.focus();
         }
+        form.addEventListener('submit', function (e) {
+            window.dataLayer = window.dataLayer || [];
+            window.dataLayer.push({
+                event: 'form_submit',
+                form_id: form.id,
+            });
+
+            // wait for submit to fire gtm event
+            e.preventDefault();
+            setTimeout(() => {
+                form.submit();
+            }, 100);
+        });
     },
 });
