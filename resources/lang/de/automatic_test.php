40f194ad34f63f30e188b0c5b22a277f76c9082e
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2025-12-22 10:06:51 +0100

    feat: add translation and icon for »none« category

--- resources/lang/de/automatic_test.php
+++ resources/lang/de/automatic_test.php
@@ -10,6 +10,7 @@ return [
         'A' => 'Probleme in Konformitätsstufe A',
         'AA' => 'Probleme in Konformitätsstufe AA',
         'AAA' => 'Probleme in Konformitätsstufe AAA',
+        'none' => 'Probleme ohne Konformitätsstufe',
         'passes' => 'Erfolgreich abgeschlossene Überprüfungen',
     ],
 
@@ -21,6 +22,7 @@ return [
         'A' => 'Diese Konformitätsstufe bildet die Basis vieler Regeln bezüglich a11y. Die Probleme sollten auf jeden Fall verbessert werden.',
         'AA' => 'Wie die Konformitätsstufe A, ist auch dieses Level gesetzlich einzuhalten, sodass auch diese Probleme in Augenschein genommen werden sollten.',
         'AAA' => 'Diese Konformitätsstufe ist nicht gesetzlich vorgeschrieben, aber verbessert weiterhin die User Experience.',
+        'none' => 'Bei diesen entdeckten Probleme handelt es sich um »Best Practices«, die eingehalten werden sollten',
         'passes' => 'Folgende Überprüfungen wurden bereits erfolgreich abgeschlossen und müssen nicht weiter angepasst werden.',
     ],
 ];
